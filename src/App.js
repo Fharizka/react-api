a25cfdf7e8cc3f9279386db26141fa285b833cc9
fharizka <s.rizkayolanda@gmail.com>
2020-11-04 19:51:54 +0700

    live coding afternoon class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react"
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      keyword: "",
+      film:[]
+    }
+  }
+
+  cariFilm= () => {
+    // console.log("ini filmnya")
+    // console.log(this.state.keyword)
+    fetch(`http://www.omdbapi.com/?apikey=6d1f3e3e&s=${this.state.keyword}`)
+      .then(response => response.json())
+      .then(result => {
+        this.setState({
+          film: result.Search
+        })
+      })
+
+  }
+
+  namaFilm = (event) => {
+    this.setState({
+      keyword: event.target.value
+    })
+  }
+
+  render(){
+    return(
+      <>
+      <h1>ini fentch api</h1>
+      <input type="text" onChange={this.namaFilm}></input>
+      <button onClick={this.cariFilm}>klik</button>
+        <>
+          {this.state.film.map(post => (
+            <div key={post.imdbID}>
+              <h2>{post.Title}</h2>
+              <img src={post.Poster}></img>
+            </div>
+          )
+          ) } 
+        </>
+      </>
+    )
+  }
+
 }
+
 
 export default App;
